3f3e660c44a10c2ad3122498539705cd0bd78501
jfores <fores.martos.jaume@gmail.com>
2021-10-22 11:03:37 +0200

    Added dataset documentation

--- R/data.R
+++ R/data.R
@@ -1,12 +1,21 @@
-#' GSE42568_Collapsed
+#' GEO_Eset_Norm_frma_Collapsed
 #'
-#' A gene expression dataset of 104 breast cancer samples and 17 healthy breast tissues. Contains a data.frame storing the gene
-#' expression values of the 121 samples and an additional data.frame storing information about covariates linked to each sample.
+#' A matrix containing gene expression data of 104 breast cancer samples and 17 healthy breast tissues.
 #'
-#' @format Contains two objects. A gene expression dataframe and an phenotype data dataframe.
+#' @format Gene expression matrix.
 #' \describe{
 #'   \item{GEO_Eset_Norm_frma_Collapsed}{Gene expression of 104 breast cancer samples and 17 healthy tissue samples}
-#'   \item{pData_FRMA}{Covariate data of the included samples}
 #'   ...
 #' }
-"GSE42568_Collapsed"
+"GEO_Eset_Norm_frma_Collapsed"
+
+#' pData_FRMA
+#'
+#' A data.frame containing covariate data for the 104 breast cancer samples and 17 healthy breast tissues.
+#'
+#' @format Dataframe with covariate data.
+#' \describe{
+#'   \item{pData_FRMA}{Covariate data for the 104 breast cancer samples and 17 healthy tissue samples}
+#'   ...
+#' }
+"pData_FRMA"
