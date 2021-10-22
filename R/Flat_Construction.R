e981cacc88fab6e3c44dd61f118bfdc47defaf70
jfores <fores.martos.jaume@gmail.com>
2021-10-22 12:06:34 +0200

    Doccumented normal_tissue_data

--- R/Flat_Construction.R
+++ R/Flat_Construction.R
@@ -1,9 +1,16 @@
-
-
-flatten_healthy <- function(normal_tissue_data){
-  df_out <- normal_tissue_data
-  for(i in 1:ncol(normal_tissue_data)){
-    df_out[,i] <- fitted(lm(normal_tissue_data[,i] ~ 0 + ., data = data.frame(normal_tissue_data)[,-i]))
+#' F
+#'
+#' @param normal_tissue_data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' flatten_normal_tiss(normal_tissue_matrix)
+flatten_normal_tiss <- function(normal_tiss){
+  df_out <- normal_tiss
+  for(i in 1:ncol(normal_tiss)){
+    df_out[,i] <- fitted(lm(normal_tiss[,i] ~ 0 + ., data = data.frame(normal_tiss)[,-i]))
   }
   return(df_out)
 }
