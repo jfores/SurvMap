2634f1bcb164da1b7ba42ce262cf6c9e04d4e058
jfores <fores.martos.jaume@gmail.com>
2021-10-22 12:08:40 +0200

    flatten_normal_tiss has been changed

--- R/Flat_Construction.R
+++ R/Flat_Construction.R
@@ -10,7 +10,7 @@
 flatten_normal_tiss <- function(normal_tiss){
   df_out <- normal_tiss
   for(i in 1:ncol(normal_tiss)){
-    df_out[,i] <- fitted(lm(normal_tiss[,i] ~ 0 + ., data = data.frame(normal_tiss)[,-i]))
+    df_out[,i] <- stats::fitted(stats::lm(normal_tiss[,i] ~ 0 + ., data = data.frame(normal_tiss)[,-i]))
   }
   return(df_out)
 }
