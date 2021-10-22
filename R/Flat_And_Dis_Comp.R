3938d645f79ed9d9126324863d64d65608009e7a
jfores <fores.martos.jaume@gmail.com>
2021-10-22 12:38:52 +0200

    The file were flattening and disease vector generation functions are stored has been changed to Flat_And_Dis_Comp

---
+++ R/Flat_And_Dis_Comp.R
@@ -0,0 +1,19 @@
+#' Flatten normal tissues
+#'
+#' Given a matrix which contains the expression values of n healthy tissue samples produces the flattened vector matrix
+#' as reported in  Disease-specific genomic analysis: identifying the signature of pathologic biology.
+#'
+#' @param normal_tiss A normal tissue data gene expression matrix.
+#'
+#' @return A gene expression matrix containing the flattened versión of the vectors.
+#' @export
+#'
+#' @examples
+#' flatten_normal_tiss(normal_tissue_matrix)
+flatten_normal_tiss <- function(normal_tiss){
+  df_out <- normal_tiss
+  for(i in 1:ncol(normal_tiss)){
+    df_out[,i] <- stats::fitted(stats::lm(normal_tiss[,i] ~ 0 + ., data = data.frame(normal_tiss)[,-i]))
+  }
+  return(df_out)
+}
--- R/Flat_Construction.R
+++ R/Flat_Construction.R
@@ -1,16 +1,1 @@
-#' F
-#'
-#' @param normal_tiss A normal tissue data gene expression matrix.
-#'
-#' @return A gene expression matrix containing the flattened versión of the vectors.
-#' @export
-#'
-#' @examples
-#' flatten_normal_tiss(normal_tissue_matrix)
-flatten_normal_tiss <- function(normal_tiss){
-  df_out <- normal_tiss
-  for(i in 1:ncol(normal_tiss)){
-    df_out[,i] <- stats::fitted(stats::lm(normal_tiss[,i] ~ 0 + ., data = data.frame(normal_tiss)[,-i]))
-  }
-  return(df_out)
-}
+
