2fda1c24f7c5cbebeb0900561235fded0c0043dd
jfores <fores.martos.jaume@gmail.com>
2021-11-10 10:33:12 +0100

    Adding plotting functions

---
+++ R/Plotting_Functions.R
@@ -0,0 +1,16 @@
+
+
+
+
+map_to_color <- function(x,limits=NULL){
+  pallette_ob <-  grDevices::colorRampPalette(colors = c("blue","red"))(100)
+  if(is.null(limits)){
+    limits=range(x)}
+  map_to_col <- pallette_ob[base::findInterval(x,base::seq(limits[1],limits[2],length.out=length(pallette_ob)+1), all.inside=TRUE)]
+  return(map_to_col)
+  }
+
+
+plot_mapper <- function(mapper_list){
+  NULL
+}
